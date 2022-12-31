95a5a0fe744c92dec89a91f293c867914c35ac92
Pranjal Agarwal <pranjalagarwal6@gmail.com>
2022-12-31 15:26:16 +0530

    okok

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,6 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            CurrentAccount obj = new CurrentAccount("Erwin", 6000, "YZX");
-            System.out.println(obj.getTradeLicenseId());
-            obj.withdraw(500);
-            obj.deposit(200);
-            obj.validateLicenseId();
-            System.out.println(obj.getBalance());
 
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
     }
 }
